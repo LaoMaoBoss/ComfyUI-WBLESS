d2c29fd40498c936bcced63d6833ff2cdcf788dd
LaoMaoBoss <wuboboss666@gmail.com>
2025-07-30 11:48:28 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
+import sys
 from pathlib import Path
+
+WBLESS_LIB_PATH = Path(__file__).resolve().parent / "lib"
+if str(WBLESS_LIB_PATH) not in sys.path:
+    sys.path.insert(0, str(WBLESS_LIB_PATH))
 
 from cozy_comfyui.node import \
     loader
